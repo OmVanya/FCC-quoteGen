640cac59e2b7d21648dd10ec5b96967181c58c99
Krystal Higgins <krystalhiggins77@gmail.com>
2017-08-12 12:35:19 -0700

    adding all files

---
+++ quotes.js
@@ -0,0 +1,76 @@
+/*Quotes Array*/
+
+var quotes = [
+ 
+"\"A learning experience is one of those things that say, 'You know that thing you just did? Don't do that'.\"<br>- Douglas Adams <small><em>(The Salmon of Doubt)</em></small>",
+"\"Let us think the unthinkable, let us do the undoable, let us prepare to grapple with the ineffable itself, and see if we may not eff it after all.\"<br>- Douglas Adams <small><em>(DirkGently's Holistic Detective Agency)</em></small>",
+"\"I may not have gone where I intended to go, but I think I have ended up where I needed to be.\"<br>- Douglas Adams <small><em>(The Long Dark Tea-Time of the Soul)</em></small>",
+"\"I love deadlines. I love the whooshing noise they make as they go by.\"<br>- Douglas Adams <small><em>(The Salmon of Doubt)</em></small>",
+"\"Driving a Porsche in London is like bringing a Ming vase to a football game.\"<br>- Douglas Adams <small><em>(Don't Panic: The Official Hitchhiker's Guide to the Galaxy Companion)</em></small>",
+"\"The ships hung in the sky, much the way that bricks don't.\"<br>- Douglas Adams <small><em>(The Hitchhikers Guide To The Galaxy)</em></small>",
+"\"He attacked everything in life with a mix of extraordinary genius and naive incompetence, and it was often difficult to tell which was which.\"<br>- Douglas Adams <small><em>",
+"\"Time is an illusion. Lunchtime doubly so.\"<br>- Douglas Adams <small><em>(The Hitchhikers Guide To The Galaxy)</em></small>",
+"\"Any planet is 'Earth' to those that live on it.\"<br>- Isaac Asimov <small><em>(Pebble in the Sky)</em></small>",
+"\"Never let your sense of morals prevent you from doing what is right.\"<br>- Isaac Asimov <small><em>(Foundation)</em></small>",
+"\"Violence is the last refuge of the incompetent.\"<br>- Isaac Asimov <small><em>(Foundation)<\em></small>",
+"\"We are an impossibility in an impossible universe.\"<br>- Ray Bradbury", 
+"\"Insanity is relative. It depends on who has who locked in what cage.\"<br>- Ray Bradbury",
+"\"You don't have to burn books to destroy a culture. Just get people to stop reading.\"<br>-Ray Bradbury",
+"\"We need not to be let alone. We need to be really bothered once in a while. How long is it since you were really bothered? About something important, about something real?\"<br>- Ray Bradbury <small><em>(Fahrenheit 451)</em></small>",
+"\"Every morning I jump out of bed and step on a landmine. The landmine is me. After the explosion, I spent the rest of the day putting pieces together.\"<br>- Ray Bradbury <small><em>(Zen in the Art of Writing)</em></small>",
+"\"So few want to be rebels anymore. And out of those few, most, like myself, scare easily.\"<br>- Ray Bradbury",
+"\"Science is no more than an investigation of a miracle we can never explain, and art is an interpretation of that miracle.\"<br>- Ray Bradbury <small><em>(The Martian Chronicles)</em></small>",
+"\"Two possibilities exist: either we are alone in the Universe or we are not. Both are equally terrifying.\"<br>- Arthur C. Clarke",
+"\"How inappropriate to call this planet 'Earth', when it is clearly 'Ocean'.\"<br>- Arthur C. Clarke",
+"\"I don't believe in astrology; I'm a Sagittarius and we're skeptical.\"<br>- Arthur C. Clarke",
+"\"..science fiction is something that could happen - but usually you wouldn't want it to. Fantasy is something that couldn't happen - though often you only wish that it could.\"<br>- Arthur C. Clarke",
+"\"If you think this Universe is bad, you should see some of the others.\"<br>- Philip K. Dick",
+"\"It really seems to me that in the midst of great tragedy, there is always the possibility that something terribly funny will happen.\"<br>- Philip K. Dick",
+"\"It is sometimes an appropriate response to reality to go insane.\"<br>- Philip K. Dick <small><em>(VALIS)</em></small>",
+"\"Certainly it constitutes bad news when the people who agree with you are buggier than batshit.\"<br>- Philip K. Dick",
+"\"Deep in the human unconscious is a pervasive need for a logical universe that makes sense. But the real universe is always one step beyond logic.\"<br>- Frank Herbert <small><em>(Dune)</em></small>",
+"\"If you ask 'Should we be in space?' you ask a nonsense question. We are in space. We will be in space.\"<br>- Frank Herbert",
+"\"I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration.\"<br>- Frank Herbert <small><em>(Dune)<?em></small>",
+"\"Show me a completely smooth operation and I'll show you someone who's covering mistakes. Real boats rock.\"<br>- Frank Herbert <small><em>(Chapterhouse: Dune)</em></small>",
+"\"You see, gentlemen, they have something to die for. They've discovered they're a people. They're awakening.\"<br>- Frank Herbert <small><em>(Dune)</em></small>",
+"\"Emotions are the curse of logic.\"<br>- Frank Herbert <small><em>(The Green Brain)</em></small>",
+"\"Absolute power does not corrupt absolutely, absolute power attracts the corruptible.\"<br>- Frank Herbert",
+"\"Here's a quick rule of thumb: Don't annoy science fiction writers. These are people who destroy entire planets before lunch. Think of what they'll do to you.\"<br>- John Scalzi",
+"\"I didn't mind getting old when I was young. It's the being old now that's getting to me.\"<br>- John Scalzi",
+"\"I'm not insane sir. I have a finely calibrated sense of acceptable risk.\"<br>- John Scalzi <small><em>(Old Man's War)</em></small>",
+"\"Science, my lad, is made up of mistakes, but they are mistakes which it is useful to make, because they lead little by little to the truth.\"<br>- Jules Verne <small><em>(A Journey to the Center of the Earth)</em></small>",
+"\"The moon, by her comparative proximity, and the constantly varying appearances produced by her several phases, has always occupied a considerable share of the attention of the inhabitants of the Earth.\"<br>- Jules Verne <small><em>(From the Earth to the Moon)</em></small>",
+"\"I believe cats to be spirits come to Earth. A cat, I am sure, could walk on a cloud without coming through.\"<br>- Jules Verne",
+"\"Those who believe in telekinetics, raise my hand.\"<br>- Kurt Vonnegut",
+"\"Any reviewer who expresses rage and loathing for a novel is preposterous. He or she is like a person who has put on full battle armor and attacked a hot fudge sundae.\"<br>- Kurt Vonnegut",
+"\"Everything was beautiful and nothing hurt.\"<br>- Kurt Vonnegut <small><em>(Slaughterhouse-Five)</em></small>",
+"\"I do feel that evolution is being controlled by some sort of divine engineer. I can't help thinking that. And this engineer knows exactly what he or she is doing and why, and where evolution is headed. That's why we've got giraffes and hippopotami and the clap.\"<br>- Kurt Vonnegut",
+"\"Science is magic that works.\"<br>- Kurt Vonnegut <small><em>(Cat's Cradle)</em></small>",
+"\"If your brains were dynamite there wouldn't be enough to blow your hat off.\"<br>- Kurt Vonnegut <small><em>(Timequake)</em></small>",
+"\"Poo-tee-weet\"<br>- Kurt Vonnegut <small><em>(Slaughterhouse-Five)</em></small>",
+"\"We are what we pretend to be, so we must be careful about what we pretend to be.\"<br>- Kurt Vonnegut",
+"\"Looking at these stars suddenly dwarfed my own troubles and all the gravities of terrestrial life.\"<br>-  H.G. Wells <small><em>(The Time Machine)</em></small>",
+"\"In my experience, there is no such thing as luck.\"<br>- Obi-Wan Kenobi <small><em>(Star Wars Episode IV: A New Hope)</em></small>",
+"\"You don't just give up. You don't just let things happen. You make a stand! You say no! You have the guts to do what's right, even when everyone else just runs away.\"<br>- Rose Tyler <small><em>(Doctor Who)</em></small>",
+"\"The needs of the many outweigh the needs of the few.\"<br>- Spock <small><em>(Star Trek II: The Wrath of Khan)<\em></small>",
+"\"KHHHAAAAAAAANNNN!!!\"<br>- James T. Kirk <small><em>(Star Trek II: The Wrath  of Khan)</em></small>",
+"\"Sometimes you gotta run before you can walk.\"<br>- Tony Stark <small><em>(Iron Man)</em></small>",
+"\"Never ignore a coincidence. Unless you're busy. In which case, always a ignore coincidence.\"<br>- The Doctor <small><em>(Doctor Who)</em></small>",
+"\"You are what you do. A man is defined by his actions, not his memory.\"<br>- Kuato <small><em>(Total Recall)</em></small>",
+"\"Live long and prosper.\"<br>- Spock <small><em>(Star Trek)</em></small>",
+"\"Do what I do. Hold tight and pretend it's a plan!\"<br>- The Doctor <small><em>(Doctor Who)</em></small>",
+"\"If we're going to be damned, let's be damned for what we really are.\"<br>- Jean Luc Picard <small><em>(Star Trek)</em></small>",
+"\"Some people live more in 20 years than others do in 80. It\'s not the time that matters, it\'s the person.\"<br>- The Doctor <small><em>(Doctor Who)</em></small>",
+"\"Try not. Do... or do not. There is no try.\"<br>- Yoda <small><em>(Star Wars Episode V: The Empire Strikes Back)</em></small>",
+"\"Time is a companion that goes with us on a journey. It reminds us to cherish each moment, because it will never come again. What we leave behind is not as important as how we have lived.\"<br>- Jean Luc Picard <small><em>(Star Trek)</em></small>",
+]
+newQuote();
+
+function newQuote(){
+	var randomNum = Math.floor(Math.random() * (quotes.length));
+	document.getElementById("quote").innerHTML = quotes[randomNum];
+	var tweetQuote = quotes[randomNum].split(' ').join('%20');
+  tweetQuote = tweetQuote.split('<br>').join('');
+  tweetQuote = "https://twitter.com/intent/tweet?text=" + tweetQuote.split('"').join('')
+  $('.twitter-share-button').attr('href', tweetQuote);
+};
